18dbb9de0bf268423ad8f1bede20921693b6c184
Jeff Brown <jeff@jeffbrown.tech>
2024-04-16 05:45:07 -0700

    initial commit

---
+++ azcli/backend.hcl
@@ -0,0 +1,5 @@
+resource_group_name = "tfstate-demo-rg"
+storage_account_name = "jbttfstatedemosa"
+container_name = "tfstate"
+key = "azcli-demo.tfstate"
+use_azuread_auth = true
---
+++ azcli/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7"
+    }
+
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">= 1.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/JeffBrownTech"
+  use_oidc        = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "azcli-rg"
+  location = "westus"
+}
+
+resource "azuredevops_project" "example" {
+  name               = "AzCLI Example"
+  visibility         = "private"
+  version_control    = "Git"
+  work_item_template = "Agile"
+  description        = "AzCLI Example - Managed by Terraform"
+}
